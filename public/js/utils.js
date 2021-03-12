bcc51a8d3d8e4bab72d7bd049b9911fc672309e6
stan055 <stanislav055@gmail.com>
2021-03-12 09:33:07 +0200

    fix

--- public/js/utils.js
+++ public/js/utils.js
@@ -27,40 +27,4 @@ function setUrlNewPi (url, pi) {
   }
 
 
-  function createRow (element) {
-    let newRow = tableRef.insertRow(0);
-    
-    let attributesStr = element.attributes.map(e => {
-      atrStr = ' ' + e.key.name + ': ' + e.value.name;
-      return atrStr;
-    }).toString();
 
-    let size = element.size.map(e => {
-      sizeStr = e.value + ' (' + 'stock:' + e.stock + ') ';
-      return sizeStr;
-    }).toString();
-
-    newRow.innerHTML = 
-      `<tr>
-        <th scope="row">${productCounter++}</th>
-        <td>${element.name}</td>
-        <td><a href="${element.url}">${element.url}</a></td>
-        <td>${element.brand}</td>
-        <td>${element.code}<br>${element.id}</td>
-        <td>
-          originalPrice: ${element.price.originalPrice.text}, 
-          <br>
-          discountedPrice: ${element.price.discountedPrice.text}
-        </td>
-        <td>
-          ${size}
-        </td>
-        <td>${attributesStr}</td>
-        <td>${element.time}</td>
-      </tr>`;
-
-      // Delete row if length > 50
-      if (tableRef.rows.length > 50) {
-        tableRef.deleteRow(tableRef.rows.length - 1);
-      }
-  }
