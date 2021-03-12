0dcdde06c031516bdf9fcf34beea93033a4aa882
stan055 <stanislav055@gmail.com>
2021-03-12 20:25:51 +0200

    fix

--- public/index.js
+++ public/index.js
@@ -2,7 +2,6 @@ const btnStart = document.getElementById('btnStart');
 const inputUrl = document.getElementById('inputUrl');
 const resultHeader = document.getElementById('resultHeader');
 const loader = document.getElementById('loader');
-var setting = { display: false };
 const settingBrowser = document.getElementById('browser');
 const btnStartList = document.querySelector('#btnStartList');
 const textArea = document.querySelector('#inputUrlList');
@@ -11,7 +10,7 @@ const categoryBtnStop = document.getElementById('categoryBtnStop');
 const listBtnStop = document.getElementById('listBtnStop');
 const pattUrl = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
 const downloadDataLink = document.getElementById('downloadDataLink');
-
+var setting = { display: false };
 
 let productCounter = 0;
 let btnStopSwitch = false;
